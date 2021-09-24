874ca86fb2c801359c11988c7a24956da90f60a3
1968255106@qq.com <1968255106@qq.com>
2021-09-24 17:11:13 +0800

    增加用户信息的查询

--- src/main/java/com/ggg/mybatisplus/service/impl/UserInfoServiceImpl.java
+++ src/main/java/com/ggg/mybatisplus/service/impl/UserInfoServiceImpl.java
@@ -4,7 +4,10 @@ import com.ggg.mybatisplus.entity.UserInfo;
 import com.ggg.mybatisplus.mapper.UserInfoMapper;
 import com.ggg.mybatisplus.service.IUserInfoService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +19,12 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> implements IUserInfoService {
+
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
+    List<UserInfo> selectAll(){
+        return userInfoMapper.selectList(null);
+    }
 
 }
